264670978922766a4f2649211a8157648f6ae1f0
Crazio_Chae <cowjdqo@naver.com>
2022-01-26 16:52:49 +0900

    220126 / 16:52 / 채정배
    메인으로 연결

--- app/src/main/java/com/example/project_02/MainActivity.java
+++ app/src/main/java/com/example/project_02/MainActivity.java
@@ -1,14 +1,14 @@
-package com.example.project_mirror;
-
-import androidx.appcompat.app.AppCompatActivity;
+package com.example.project_02;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import androidx.appcompat.app.AppCompatActivity;
 
+public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
