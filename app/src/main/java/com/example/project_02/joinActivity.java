2710bd4daf14b5dc35f09c91e005e41d6b4b80c2
Crazio_Chae <cowjdqo@naver.com>
2022-01-27 09:39:11 +0900

    220127 / 09:40 / 채정배
    join, Manifest, camera, Frag2, Main 수정

---
+++ app/src/main/java/com/example/project_02/joinActivity.java
@@ -0,0 +1,15 @@
+package com.example.project_02;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class joinActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_join);
+
+    }
+}
