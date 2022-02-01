f35d8e4bae427a5dbb7e18983425cb7dd3194562
Crazio_Chae <cowjdqo@naver.com>
2022-02-01 15:23:49 +0900

    220201 / 15:22 / 채정배 / 커밋

--- app/src/main/java/com/example/project_02/GuideFragment_1.java
+++ app/src/main/java/com/example/project_02/GuideFragment_1.java
@@ -12,7 +12,6 @@ import android.view.ViewGroup;
 public class GuideFragment_1 extends Fragment {
 
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
