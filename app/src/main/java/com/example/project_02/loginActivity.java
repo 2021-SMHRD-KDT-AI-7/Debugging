0fa8fc41ce5265b112712d90d473644fd8be9c19
Crazio_Chae <cowjdqo@naver.com>
2022-02-04 14:50:39 +0900

    220204 / 14:50 / 채정배 / 커밋

--- app/src/main/java/com/example/project_02/loginActivity.java
+++ app/src/main/java/com/example/project_02/loginActivity.java
@@ -24,8 +24,6 @@ public class loginActivity extends AppCompatActivity {
     private Button btn_login, btn_join;
     ImageView back;
     CheckBox cb_;
-    StringRequest sr;
-    RequestQueue rq;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,30 +40,6 @@ public class loginActivity extends AppCompatActivity {
             Intent intent = new Intent(
                     loginActivity.this, MainActivity.class);
             startActivity(intent);
-            /*sr = new StringRequest(Request.Method.POST,
-                    "링크 입력 요망",
-                    response -> {
-                        if (response.equals("true")) {
-                            Toast.makeText(getApplicationContext(),
-                                    "로그인되었습니다.", Toast.LENGTH_SHORT).show();
-                            Intent intent = new Intent(
-                                    loginActivity.this, myActivity.class);
-                            startActivity(intent);
-                        } else {
-                            Toast.makeText(getApplicationContext(),
-                                    "아이디 혹은 비밀번호를 확인해주세요.", Toast.LENGTH_SHORT).show();
-                        }
-                    },
-                    error -> {
-                    }) {
-                protected Map<String, String> getParams() {
-                    Map<String, String> data = new HashMap<>();
-                    data.put("user_id", et_id.getText().toString());
-                    data.put("user_pw", et_pw.getText().toString());
-                    return data;
-                }
-            };
-            rq.add(sr);*/
             finish();
         });
         btn_join.setOnClickListener(view -> {
