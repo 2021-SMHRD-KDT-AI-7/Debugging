2ff0bc3cdb8386ce5664a020560f02dc60abef6a
요한 <asdf>
2022-02-04 12:28:06 +0900

    220204 / 12:27 / 요한 이미지 url로 받아와 창에 보여주는 창 커밋

--- app/src/main/java/com/example/project_02/CosAdapter.java
+++ app/src/main/java/com/example/project_02/CosAdapter.java
@@ -3,6 +3,8 @@ package com.example.project_02;
 import android.app.Activity;
 import android.app.Fragment;
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,6 +12,11 @@ import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.Arrays;
 
@@ -26,7 +33,8 @@ public class CosAdapter extends BaseAdapter {
     private Context context;  // Activity 에서 보내준 화면정보 / 화면을 구성하는 정보를 담고있는 객체(Activity 핵심) MainActivity 일듯??
     private LayoutInflater inflater; // 추출한 inflater를 저장할 공간
     private Fragment_tab4 activity;
-    int[] cos_sample_list = {R.drawable.cos_sample, R.drawable.cos_sample2, R.drawable.cos_sample3, R.drawable.cos_sample4, R.drawable.cos_sample5};
+    Bitmap bitmap;
+    // int[] cos_sample_list = {R.drawable.cos_sample, R.drawable.cos_sample2, R.drawable.cos_sample3, R.drawable.cos_sample4, R.drawable.cos_sample5};
 
     public CosAdapter(int template, ArrayList<CosVO> data, Context context, Fragment_tab4 activity) {
         this.template = template;
@@ -49,7 +57,7 @@ public class CosAdapter extends BaseAdapter {
     // 5. DB img_view >> url
 
     @Override
-    public int getCount() {  // 출력할 view의 개수    = VO의 개수
+    public int getCount() {  // 출력할 view의 개수 = VO의 개수
         return data.size();
     }
 
@@ -63,43 +71,105 @@ public class CosAdapter extends BaseAdapter {
         return i;
     }
 
+    public void run(String img_url) {
+        try {
+            URL url = new URL(img_url);
+
+            // Web에서 이미지를 가져온 뒤
+            // ImageView에 지정할 Bitmap을 만든다
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setDoInput(true); // 서버로 부터 응답 수신
+            conn.connect();
+
+            InputStream is = conn.getInputStream(); // InputStream 값 가져오기
+            bitmap = BitmapFactory.decodeStream(is); // Bitmap으로 변환
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
     @Override
     public View getView(int i, View view, ViewGroup viewGroup) {
 
-        ArrayList<Integer> DRPT = new ArrayList<>(Arrays.asList(1,2,3,4,5));
-        ArrayList<Integer> DRNT = new ArrayList<>(Arrays.asList(6,7,8,9,10));
-        ArrayList<Integer> DSPT = new ArrayList<>(Arrays.asList(11,12,13,14,15));
-        ArrayList<Integer> DSNT = new ArrayList<>(Arrays.asList(16,17,18,19,20));
-        ArrayList<Integer> DRPW = new ArrayList<>(Arrays.asList(21,22,23,24,25));
-        ArrayList<Integer> DRNW = new ArrayList<>(Arrays.asList(26,27,28,29,30));
-        ArrayList<Integer> DSPW = new ArrayList<>(Arrays.asList(31,32,33,34,35));
-        ArrayList<Integer> DSNW = new ArrayList<>(Arrays.asList(36,37,38,39,40));
-        ArrayList<Integer> ORPT = new ArrayList<>(Arrays.asList(41,42,43,44,45));
-        ArrayList<Integer> ORNT = new ArrayList<>(Arrays.asList(46,47,48,49,50));
-        ArrayList<Integer> OSPT = new ArrayList<>(Arrays.asList(51,52,53,54,55));
-        ArrayList<Integer> OSNT = new ArrayList<>(Arrays.asList(56,57,58,59,60));
-        ArrayList<Integer> ORPW = new ArrayList<>(Arrays.asList(61,62,63,64,65));
-        ArrayList<Integer> ORNW = new ArrayList<>(Arrays.asList(66,67,68,69,70));
-        ArrayList<Integer> OSPW = new ArrayList<>(Arrays.asList(71,72,73,74,75));
-        ArrayList<Integer> OSNW = new ArrayList<>(Arrays.asList(76,77,78,79,80));
+        ArrayList<Integer> DRPT = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5));
+        ArrayList<Integer> DRNT = new ArrayList<>(Arrays.asList(6, 7, 8, 9, 10));
+        ArrayList<Integer> DSPT = new ArrayList<>(Arrays.asList(11, 12, 13, 14, 15));
+        ArrayList<Integer> DSNT = new ArrayList<>(Arrays.asList(16, 17, 18, 19, 20));
+        ArrayList<Integer> DRPW = new ArrayList<>(Arrays.asList(21, 22, 23, 24, 25));
+        ArrayList<Integer> DRNW = new ArrayList<>(Arrays.asList(26, 27, 28, 29, 30));
+        ArrayList<Integer> DSPW = new ArrayList<>(Arrays.asList(31, 32, 33, 34, 35));
+        ArrayList<Integer> DSNW = new ArrayList<>(Arrays.asList(36, 37, 38, 39, 40));
+        ArrayList<Integer> ORPT = new ArrayList<>(Arrays.asList(41, 42, 43, 44, 45));
+        ArrayList<Integer> ORNT = new ArrayList<>(Arrays.asList(46, 47, 48, 49, 50));
+        ArrayList<Integer> OSPT = new ArrayList<>(Arrays.asList(51, 52, 53, 54, 55));
+        ArrayList<Integer> OSNT = new ArrayList<>(Arrays.asList(56, 57, 58, 59, 60));
+        ArrayList<Integer> ORPW = new ArrayList<>(Arrays.asList(61, 62, 63, 64, 65));
+        ArrayList<Integer> ORNW = new ArrayList<>(Arrays.asList(66, 67, 68, 69, 70));
+        ArrayList<Integer> OSPW = new ArrayList<>(Arrays.asList(71, 72, 73, 74, 75));
+        ArrayList<Integer> OSNW = new ArrayList<>(Arrays.asList(76, 77, 78, 79, 80));
 
         if (view == null) {
             view = inflater.inflate(template, viewGroup, false);
         }
-        ImageView img = view.findViewById(R.id.cos_img);
-
         TextView name = view.findViewById(R.id.tv_cos_name);
         TextView brand = view.findViewById(R.id.tv_cos_brand);
         TextView price = view.findViewById(R.id.tv_cos_price);
         TextView cos_count = view.findViewById(R.id.cos_count);
+        ImageView cos_img = view.findViewById(R.id.cos_img);
 
-        img.setImageResource(cos_sample_list[i]);
-
+        // img.setImageResource(cos_sample_list[i]);
         cos_count.setText(i + 1 + "");
 
         name.setText(data.get(i).getCos_name());
         brand.setText(data.get(i).getCos_brand());
         price.setText(data.get(i).getCos_price() + "원");
+
+        // ---------------------------------------------------------- URL ImageView 출력 --------------------------------------
+        // 안드로이드에서 네트워크와 관련된 작업을 할 때,
+        // 반드시 메인 Thread가 아닌 별도의 작업 Thread를 생성하여 작업해야 한다.
+        Thread mThread = new Thread() {
+            @Override
+            public void run() {
+                try {
+                    URL url = new URL(data.get(i).getCos_img());
+
+                    // Web에서 이미지를 가져온 뒤
+                    // ImageView에 지정할 Bitmap을 만든다
+                    HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                    conn.setDoInput(true); // 서버로 부터 응답 수신
+                    conn.connect();
+
+                    InputStream is = conn.getInputStream(); // InputStream 값 가져오기
+                    bitmap = BitmapFactory.decodeStream(is); // Bitmap으로 변환
+
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+
+        mThread.start(); // Thread 실행
+
+        try {
+            // 메인 Thread는 별도의 작업 Thread가 작업을 완료할 때까지 대기해야한다
+            // join()를 호출하여 별도의 작업 Thread가 종료될 때까지 메인 Thread가 기다리게 한다
+            mThread.join();
+
+            // 작업 Thread에서 이미지를 불러오는 작업을 완료한 뒤
+            // UI 작업을 할 수 있는 메인 Thread에서 ImageView에 이미지를 지정한다
+            cos_img.setImageBitmap(bitmap);
+
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        // ---------------------------------------------------------- URL ImageView 출력 --------------------------------------
 
         return view;
     }
--- app/src/main/java/com/example/project_02/Fragment_tab4.java
+++ app/src/main/java/com/example/project_02/Fragment_tab4.java
@@ -11,7 +11,6 @@ import androidx.fragment.app.Fragment;
 
 import java.util.ArrayList;
 
-
 public class Fragment_tab4 extends Fragment {
     ArrayList<CosVO> cos_list = new ArrayList<>();
     ListView lv;
@@ -28,16 +27,14 @@ public class Fragment_tab4 extends Fragment {
         // cos_list
         // 바우만 테스트 결과 = cos DB에 바우만 컬럼의 값이 일치하는 화장품 가져와서 add
 
-        //지금은 sample 데이터
-        cos_list.add(new CosVO("닥터자르트 세라마이딘 크림 50ml", "38,000", "닥터자르트", "화장품주소"));
-        cos_list.add(new CosVO("리얼베리어 익스트림 크림 50ml", "26,900", "리얼베리어", "화장품주소"));
-        cos_list.add(new CosVO("에스티 로더 갈색병 세럼 30ML", "89,400", "에스티로더", "화장품주소"));
-        cos_list.add(new CosVO("가히 김고은 멀티밤", "29,400", "가히", "화장품주소"));
-        cos_list.add(new CosVO("크리니크 모이스춰 써지 쏙보습크림 50ml", "41,000", "크리니크", "화장품주소"));
+        // sample 데이터
+        cos_list.add(new CosVO("닥터자르트 세라마이딘 크림 50ml", "38,000", "닥터자르트", "https://image.oliveyoung.co.kr/uploads/images/goods/550/10/0000/0016/A00000016073403ko.jpg?l=ko"));
+        cos_list.add(new CosVO("리얼베리어 익스트림 크림 50ml", "26,900", "리얼베리어", "https://image.oliveyoung.co.kr/uploads/images/goods/550/10/0000/0016/A00000016072107ko.jpg?l=ko"));
+        cos_list.add(new CosVO("에스티 로더 갈색병 세럼 30ML", "89,400", "에스티로더", "https://image.oliveyoung.co.kr/uploads/images/goods/550/10/0000/0014/A00000014663504ko.jpg?l=ko"));
+        cos_list.add(new CosVO("가히 김고은 멀티밤", "29,400", "가히", "https://image.oliveyoung.co.kr/uploads/images/goods/550/10/0000/0015/A00000015499111ko.jpeg?l=ko"));
+        cos_list.add(new CosVO("크리니크 모이스춰 써지 쏙보습크림 50ml", "41,000", "크리니크", "https://image.oliveyoung.co.kr/uploads/images/goods/550/10/0000/0015/A00000015887308ko.jpg?l=ko"));
 
-        CosAdapter adapter = new CosAdapter(
-                R.layout.cos_custom, cos_list, getContext().getApplicationContext(),
-                Fragment_tab4.this);
+        CosAdapter adapter = new CosAdapter(R.layout.cos_custom, cos_list, getContext().getApplicationContext(),Fragment_tab4.this);
         lv.setAdapter(adapter);
 
         // template  >> R.layout.custom  우리가 디자인한 템플릿
