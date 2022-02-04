864725e78a947f87d26cf0925598a0319e184bd2
O-oKI <gga35120@naver.com>
2022-02-04 10:45:49 +0900

    02.04)체크용 커밋

--- app/src/main/java/com/example/project_02/joinActivity.java
+++ app/src/main/java/com/example/project_02/joinActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
@@ -12,7 +13,7 @@ import android.widget.Toast;
 
 public class joinActivity extends AppCompatActivity {
 
-    private EditText id, pw, pw2, name, bd;
+    EditText id, pw, pw2, name, bd;
     ImageView back;
     Button btn_join;
 
@@ -20,6 +21,8 @@ public class joinActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_join);
+
+        setTitle("ORACLE");
 
         back = findViewById(R.id.join_back);
         id = findViewById(R.id.et_id);
@@ -29,20 +32,42 @@ public class joinActivity extends AppCompatActivity {
         bd = findViewById(R.id.et_birthday);
         btn_join = findViewById(R.id.btn_login_); // btn_join_ 아이디 중복 방지 언더바
 
-        btn_join.setOnClickListener(v -> {
-            if (!pw.getText().toString().equals(pw2.getText().toString())) {
-                Toast.makeText(getApplicationContext(),
-                        "비밀번호를 확인해주세요.", Toast.LENGTH_SHORT).show();
-            } else {
+//        btn_join.setOnClickListener(v -> {
+//            if (!pw.getText().toString().equals(pw2.getText().toString())) {
+//                Toast.makeText(getApplicationContext(),
+//                        "비밀번호를 확인해주세요.", Toast.LENGTH_SHORT).show();
+//            } else {
+//                try {
+//                    String result;
+//                    String user_id = id.getText().toString();
+//                    String user_pw = pw.getText().toString();
+//                    String user_name = name.getText().toString();
+//                    String user_bd = bd.getText().toString();
+//                    String ad = "n";
+//
+//                    registerActivity task = new registerActivity();
+//                    result = task.execute(user_id, user_pw, user_name, user_bd, ad).get();
+//                } catch (Exception e) {
+//                    Log.i("DB_test", ".....ERROR.....!");
+//                }
+//                Toast.makeText(getApplicationContext(),
+//                        "회원가입되었습니다.", Toast.LENGTH_SHORT).show();
+//                Intent intent = new Intent(joinActivity.this, frontActivity.class);
+//                startActivity(intent);
+//                finish();
+//            }
+//        });
+        btn_join.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
                 try {
+                    String result;
                     String user_id = id.getText().toString();
                     String user_pw = pw.getText().toString();
                     String user_name = name.getText().toString();
                     String user_bd = bd.getText().toString();
-                    String admin = "n";
 
                     registerActivity task = new registerActivity();
-                    task.execute(user_id, user_pw, user_name, user_bd, admin).get();
+                    result = task.execute(user_id, user_pw, user_name, user_bd).get();
                 } catch (Exception e) {
                     Log.i("DB_test", ".....ERROR.....!");
                 }
